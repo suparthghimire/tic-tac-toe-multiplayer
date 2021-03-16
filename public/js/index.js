e9bd13e3a64456b20e348cbbddea998bbb97b9e9
suparthghimire <suparthnarayanghimire2014@outlook.com>
2021-03-16 19:41:02 +0545

    Opponent and Player System

--- public/js/index.js
+++ public/js/index.js
@@ -1,17 +1,92 @@
 const socket = io();
+// chat
 
-// selectors
-const boxes = document.querySelectorAll(".box");
+let self = {
+  id: "",
+  username: "",
+  score: 0,
+};
 
-function startGame() {
-  boxes.forEach((box) => {
-    box.addEventListener("click", handleClick);
-  });
+let opponent = {
+  id: "",
+  username: "",
+  score: 0,
+};
+
+document.querySelector(".overlay").style.visibility = "visible";
+
+let username = localStorage.getItem("username-toc-tac-toe-suaprth");
+if (!username) {
+  username = prompt("Enter your Username");
+  localStorage.setItem("username-toc-tac-toe-suaprth", username);
+}
+socket.emit("joinRoom", { username });
+socket.on("userJoin", (socketid) => {
+  self.username = username;
+  self.id = socketid;
+});
+
+socket.on("allUsers", (users) => {
+  if (users.length > 1) {
+    opponent.id = users.find((user) => user.id != self.id).id;
+    opponent.username = users.find((user) => user.id != self.id).username;
+    console.log(`Self: ${self.username} - ${self.id}`);
+    console.log(`Opponent: ${opponent.username} - ${opponent.id}`);
+    // set username for opponent and self
+    setUserName(self.username, opponent.username);
+    // TODO: Make sure to enable the waiting for player to join option
+    document.querySelector(".overlay").style.visibility = "hidden";
+  }
+});
+
+function setUserName(self, opponent) {
+  document.querySelector(".self_name").innerHTML = self;
+  document.querySelector(".opponent_name").innerHTML = opponent;
 }
 
-function handleClick(e) {
-  // mark box
-  // check for win
-  // check for draw
-  // swap turn
+socket.on("userdisconnected", (socketId) => {
+  if (socketId != self.id) {
+    console.log(`${self.username} won the game`);
+    // reset board or redirect to room selection page if any
+  }
+});
+
+/* 
+------------------------
+------------------------
+------------------------
+----------CHAT----------
+------------------------
+------------------------
+------------------------
+*/
+
+socket.on("message", ({ message, username }) => {
+  createChatList(message, username);
+});
+
+const chatForm = document.querySelector("#chat-form");
+
+chatForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let message = chatForm.chatInput.value;
+  chatForm.chatInput.value = "";
+  socket.emit("message", { message, username });
+});
+
+function createChatList(message, username) {
+  let chatList = document.querySelector(".chat-messages");
+  let chatItem = document.createElement("li");
+
+  chatItem.innerHTML = `
+   <h3 class="username">
+        ${username}
+    </h3>
+    <p class="message">
+        ${message}
+    </p>
+  `;
+
+  chatItem.classList.add("chat-item");
+  chatList.appendChild(chatItem);
 }
--- server.js
+++ server.js
@@ -1,16 +1,35 @@
-const express = require("express");
 const path = require("path");
-const app = express();
-
 const http = require("http");
+const express = require("express");
+const app = express();
 const server = http.createServer(app);
-const io = require("socket.io")(server);
 
-const PORT = 3000;
+const io = require("socket.io")(server);
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, "public")));
-app.listen(PORT, () => console.log(`Server Started At Port ${PORT}`));
+server.listen(PORT, () => console.log(`Server Started At Port ${PORT}`));
+
+let users = [];
 
 io.on("connection", (socket) => {
-  io.emit("connected");
+  socket.emit("userJoin", socket.id);
+
+  socket.on("joinRoom", ({ username }) => {
+    let user = {};
+    user.username = username;
+    user.id = socket.id;
+    users.push(user);
+    console.log(users);
+    io.emit("allUsers", users);
+  });
+
+  socket.on("message", ({ message, username }) => {
+    io.emit("message", { message, username });
+  });
+  socket.on("disconnect", () => {
+    users = [];
+    io.emit("userdisconnected", socket.id);
+    // reset board or redirect to room selection page if any
+  });
 });
